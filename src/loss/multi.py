5558532057b480341f72aaa14d1549f575c0cd55
drprojects <dr91@wanadoo.fr>
2023-07-26 11:20:53 +0200

    further fix MultiLoss load_state_dict errors

--- src/loss/multi.py
+++ src/loss/multi.py
@@ -48,3 +48,42 @@ class MultiLoss(nn.Module):
         """
         for i in range(len(self)):
             self.criteria[i].weight = weight
+
+    def state_dict(self, *args, destination=None, prefix='', keep_vars=False):
+        """Normal `state_dict` behavior, except for the shared criterion
+        weights, which are not saved under `prefix.criteria.i.weight`
+        but under `prefix.weight`.
+        """
+        destination = super().state_dict(
+            *args, destination=destination, prefix=prefix, keep_vars=keep_vars)
+
+        # Remove the 'weight' from the criteria
+        for i in range(len(self)):
+            destination.pop(f"{prefix}criteria.{i}.weight")
+
+        # Only save the global shared weight
+        destination[f"{prefix}weight"] = self.weight
+
+        return destination
+
+    def load_state_dict(self, state_dict, strict=True):
+        """Normal `load_state_dict` behavior, except for the shared
+        criterion weights, which are not saved under `criteria.i.weight`
+        but under `prefix.weight`.
+        """
+        # Get the weight from the state_dict
+        old_format = state_dict.get('criteria.0.weight')
+        new_format = state_dict.get('weight')
+        weight = new_format if new_format is not None else old_format
+        for k in [f"criteria.{i}.weight" for i in range(len(self))]:
+            if k in state_dict.keys():
+                state_dict.pop(k)
+
+        # Normal load_state_dict, ignoring self.criteria.0.weight and
+        # self.weight
+        out = super().load_state_dict(state_dict, strict=strict)
+
+        # Set the weight
+        self.weight = weight
+
+        return out
